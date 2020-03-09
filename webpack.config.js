343e522d6170d95fde342f51a708c035a57d1cf2
Kota Ogino <okitsu090807@gmail.com>
2020-03-09 22:31:54 +0900

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-var debug   = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
-var path    = require('path');
+var path = require('path');
 
 module.exports = {
   context: path.join(__dirname, "src"),
   entry: "./js/client.js",
   devServer: {
-  //index.htmlがあるフォルダ名
+    //index.htmlがあるフォルダ名
     contentBase: "src",
     open: true
   },
   module: {
     rules: [{
-      test: /\.jsx?$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: [{
           loader: 'babel-loader',
@@ -20,14 +20,38 @@ module.exports = {
             presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }]
-      }]
-    },
-    output: {
-      path: __dirname + "/src/",
-      filename: "client.min.js"
-    },
-    plugins: debug ? [] : [
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+      },
+      //css/sass-loader
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          /*
+          MiniCssExtractPlugin.loader,
+          */
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          'sass-loader'
+        ],
+      },
     ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    // new MiniCssExtractPlugin({
+    //   filename: 'style.css'
+    // }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      sourcemap: false
+    }),
+  ]
 };
